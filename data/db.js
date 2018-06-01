7863057b63ffbd23aca06911ffb455156f1d5e3d
odax <nr10@albion.edu>
2018-06-01 15:00:41 -0400

    saved

--- data/db.js
+++ data/db.js
@@ -1,7 +1,2 @@
 const mongoose = require('mongoose');
 
-module.exports = {
-  connectTo: function(database = 'sandbox', host = 'localhost') {
-    return mongoose.connect(`mongodb://odax:sandmonster22@ds139920.mlab.com:39920/noteex`);
-  },
-};
